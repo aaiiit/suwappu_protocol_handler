d9709c520f3cffa7d51b3d877d4e3d0b86a49ae0
Aaiiit <tom@gameswap.be>
2013-05-02 15:56:05 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,29 @@
+== README
+
+Application used to handle suwappu:// protocol.
+
+
+== Prerequisities
+
+	*	Barcode printer must be installed
+	* Chrome
+
+
+== Installation
+
+	*	Download
+	*	Make sure handler.rb is executable
+	* Create suwappu.desktop in /usr/share/applications/
+	* Configure correct paths in suwappu.desktop and handler.rb
+
+
+== suwappu.desktop
+[Desktop Entry]
+Type=Application
+Name=Suwappu
+GenericName=Suwappu
+Exec=/home/tom/Code/suwappu_protocol_handler/handler %U
+Icon=arduino
+Terminal=false
+Categories=Development;Engineering;Electronics;
+MimeType=text/x-suwappu;x-scheme-handler/suwappu;
---
+++ handler.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/env ruby
+
+require 'logger'
+
+file = File.open('./log/suwappu.log', File::WRONLY | File::APPEND)
+@logger = Logger.new(file)
+@logger.level = Logger::INFO
+
+def analyze_uri(uri)
+	command_and_uri = uri.split(/\/\//)[1]
+	if command_and_uri.start_with?('print/')
+		# Print Command Invoked
+		pdf_path = command_and_uri['print/'.length,command_and_uri.length-1]
+		print_barcode download("http://#{pdf_path}")
+	end
+end
+
+def download(uri)
+	file_name = "barcode-#{Time.now.to_i}.pdf"
+	@logger.info "Downloading #{uri} --> /home/tom/Code/suwappu_protocol_handler/tmp/documents/#{file_name}"
+	cmnd = "curl #{uri} -o './tmp/documents/#{file_name}'" 
+	@logger.info cmnd
+	system cmnd
+	file_name
+end
+
+def print_barcode(local_doc)
+	cmnd = "lpr -P 'Zebra-LP2824' ./tmp/documents/#{local_doc}"
+	@logger.info cmnd
+	system cmnd
+end
+
+
+argument = ARGV[0]
+puts argument
+puts analyze_uri(argument)
+@logger.close
+
