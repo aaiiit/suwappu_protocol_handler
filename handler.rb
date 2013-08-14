d14657b1c883fbab0acb1d89831ea1a15a9c579d
Aaiiit <tom@gameswap.be>
2013-08-14 15:27:43 +0200

    -

--- handler.rb
+++ handler.rb
@@ -1,10 +1,18 @@
-#!/usr/bin/env ruby
+#!/home/tom/.rvm/rubies/ruby-1.9.2-p320/bin/ruby
 
 require 'logger'
+require 'pusher'
+require '/home/tom/Code/pusher-ruby-client'
+#require 'pusher-client'
 
 file = File.open('/home/tom/Code/suwappu/suwappu_protocol_handler/log/suwappu.log', File::WRONLY | File::APPEND)
 @logger = Logger.new(file)
 @logger.level = Logger::INFO
+Pusher.app_id = 45972
+Pusher.key = '71ea26969299b55eca1a'
+Pusher.secret = '063c8f72e55c99814a02' 
+Pusher.host = 'pusher'
+Pusher.port = 4567
 
 def analyze_uri(uri)
 	command_and_uri = uri.split(/\/\//)[1]
@@ -28,6 +36,18 @@ def analyze_uri(uri)
 	else
 		puts "Command #{command} not found!"
 	end
+
+	
+end
+
+def event_completed(command,argument)
+	begin
+		Pusher['suwappu_handler'].trigger("#{command}_completed",argument)
+		@logger.info "Event suwappu_handler##{command}_completed called"
+	rescue Pusher::Error => e
+		puts e
+		@logger.debug e
+	end
 end
 
 def download(uri)
@@ -43,6 +63,7 @@ def print_barcode(local_doc)
 	cmnd = "lpr -P 'Zebra-LP2824' /home/tom/Code/suwappu/suwappu_protocol_handler/tmp/documents/#{local_doc}"
 	@logger.info cmnd
 	system cmnd
+	event_completed('print_barcode',{file: local_doc})
 end
 
 def storage_slot(device,slot)
@@ -54,15 +75,16 @@ def storage_slot(device,slot)
 end
 
 def take_picture(swap_number)
-	cmnd = "streamer -o /tmp/#{swap_number}.jpeg && scp /tmp/#{swap_number}.jpeg deployer@inventorycontrol:/u/apps/inventorycontrol/shared/images/"
+	cmnd = "streamer -o /tmp/#{swap_number}.jpeg && scp /tmp/#{swap_number}.jpeg deployer@inventorycontrol:/u/apps/inventorycontrol/shared/suwappu_pictures/"
 	@logger.info "take_picture"
 	@logger.info cmnd
 	system cmnd
+	event_completed('take_picture',{swap_number: swap_number,file: "file:///tmp/#{swap_number}.jpeg"})
 end
 
-def scan(swap_number)
+def scan(uid)
 	scanner = 'hpaio:/net/HP_Color_LaserJet_2840?ip=192.168.11.31'
-	file = "/tmp/#{swap_number}"
+	file = "/tmp/#{uid}"
 	cmnd = "scanimage -d #{scanner}	--resolution=300 > #{file}.pnm"
 	@logger.info "scan"
 	@logger.info cmnd
@@ -73,6 +95,9 @@ def scan(swap_number)
 	cmnd = "ps2pdf #{file}.ps #{file}.pdf"
 	@logger.info cmnd
 	system cmnd
+	#event_completed('scan',{uid: uid,file: "#{file}.pdf"})
+	event_completed('scan',{uid: uid,file: "#{file}.pdf"})
+	"#{file}.pdf"
 end
 
 argument = ARGV[0]
